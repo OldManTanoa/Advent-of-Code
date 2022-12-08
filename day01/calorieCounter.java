6e3758555d6b4883062e32e711f6001991faa58e
OldManTanoa <zogg.ryan@gmail.com>
2022-12-08 09:01:02 +0100

    Finish day 1

--- day01/calorieCounter.java
+++ day01/calorieCounter.java
@@ -8,6 +8,8 @@ public class calorieCounter {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(new File("C:\\Users\\zoggr\\OneDrive\\Big comp\\Dokumente\\VSS\\Git projects\\Advent-of-Code\\day01\\fileInput.txt"));
         int max = 0;
+        int max2 = 0;
+        int max3 = 0;
         int currentTotal = 0;
         String line = input.nextLine();
         Scanner lineread = new Scanner(line);
@@ -16,20 +18,39 @@ public class calorieCounter {
                 int next = lineread.nextInt();
                 System.out.println("Looking at "+next);
                 currentTotal+= next;
-                if (currentTotal > max) {
-                    max = currentTotal;
+                if (currentTotal > max3) {
+                    if (currentTotal > max2) {
+                        if (currentTotal > max) {
+                            max3 = max2;
+                            max2 = max;
+                            max = currentTotal;
+                        } else {
+                            max3 = max2;
+                            max2 = currentTotal;
+                        }
+                    } else {
+                        max3 = currentTotal;
+                    }
                 }
 
+                try{
                 line = input.nextLine();
                 lineread = new Scanner(line);
+                } catch (Exception e) {
+
+                }
             }
             
+            try{
             currentTotal = 0;
             line = input.nextLine();
             lineread = new Scanner(line);
+            } catch (Exception e) {
+
+            }
         }
 
-        System.out.println("Max is " + max);
+        System.out.println("Max is " + (max3+max2+max));
         lineread.close();
         input.close();
     }
