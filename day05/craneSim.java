6a98233ec8dc22604c83a2580f3afed40bdfa277
OldManTanoa <zogg.ryan@gmail.com>
2022-12-12 12:40:25 +0100

    Start day 05

---
+++ day05/craneSim.java
@@ -0,0 +1,20 @@
+package day05;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class craneSim {
+    
+    public static void main(String args[]) {
+
+    }
+
+    public static String topOfStack(File file) throws FileNotFoundException {
+        Scanner input = new Scanner(file);
+
+        
+
+        return "";
+    }
+}
