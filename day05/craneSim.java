97421defb86aec8ecf8d8820c8b3a53f8faee55a
OldManTanoa <zogg.ryan@gmail.com>
2022-12-12 12:49:56 +0100

    day 05

--- day05/craneSim.java
+++ day05/craneSim.java
@@ -6,14 +6,21 @@ import java.io.FileNotFoundException;
 
 public class craneSim {
     
-    public static void main(String args[]) {
-
+    public static void main(String args[]) throws FileNotFoundException {
+        System.out.println("Solution to part 1 is "+topOfStack(new File("day05\\fileInput.txt")));
     }
 
     public static String topOfStack(File file) throws FileNotFoundException {
         Scanner input = new Scanner(file);
+        String[] platform;
+        String line;
+        int a = 1;
 
-        
+        while(a == 1) {
+            line = input.nextLine();
+            System.out.println(line);
+            a= 0;
+        }
 
         return "";
     }
