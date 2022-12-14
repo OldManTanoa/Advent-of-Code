b0ebb8dd197cb85309b59824a005b2f7b90172e0
OldManTanoa <zogg.ryan@gmail.com>
2022-12-14 13:09:44 +0100

    start day 06

--- day05/craneSim.java
+++ day05/craneSim.java
@@ -10,11 +10,11 @@ public class craneSim {
     
     public static void main(String args[]) throws FileNotFoundException {
         System.out.println("Solution to part 1 is "+topOfStack(new File("day05\\fileInput.txt")));
+        System.out.println("Solution to part 2 is "+topOfStack2(new File("day05\\fileInput.txt")));
     }
 
     public static String topOfStack(File file) throws FileNotFoundException {
         Scanner input = new Scanner(file);
-        String[] platform;
         String line;
         int highest = 0;
 
@@ -34,6 +34,7 @@ public class craneSim {
 				String toAdd = line.substring(1+4*i, 2+4*i);
 				if (!toAdd.equals(" ")) {
 					stack.stacks.get(i).addLast(toAdd);
+                    stack.print();
 				}
 			}
 		}
@@ -56,6 +57,55 @@ public class craneSim {
                 stack.move(from-1, to-1);
             }
         }
+
+        input.close();
+        return stack.getSol();
+    }
+
+    public static String topOfStack2(File file) throws FileNotFoundException {
+        Scanner input = new Scanner(file);
+        String line;
+        int highest = 0;
+
+        while((line = input.nextLine()).charAt(1) != '1') {
+            highest++;
+            continue;
+        }
+
+        int stackNumber = line.split("   ").length;
+
+        input = new Scanner(file);
+
+        Stacks stack = new Stacks(stackNumber);
+
+        while (input.hasNextLine() && (line = input.nextLine()).charAt(1) != '1') {
+			for (int i = 0; i < stackNumber; i++) {
+				String toAdd = line.substring(1+4*i, 2+4*i);
+				if (!toAdd.equals(" ")) {
+					stack.stacks.get(i).addLast(toAdd);
+                    stack.print();
+				}
+			}
+		}
+        
+        //skip a line
+        input.nextLine();
+        
+        while (input.hasNext()) {
+            //skip move
+            input.next();
+            int amount = input.nextInt();
+            //skip from
+            input.next();
+            int from = input.nextInt();
+            //skip to
+            input.next();
+            int to = input.nextInt();
+
+            stack.moveMany(from-1, to-1, amount);
+        }
+
+        input.close();
         return stack.getSol();
     }
 }
@@ -72,13 +122,25 @@ class Stacks {
     }
 
     void move(int from, int to) {
-        String moved = stacks.get(from).removeLast();
+        String moved = stacks.get(from).removeFirst();
 
-        stacks.get(to).addLast(moved);
+        stacks.get(to).addFirst(moved);
+    }
+
+    void moveMany(int from, int to, int amount) {
+        LinkedList<String> moved = new LinkedList<String>();
+
+        for (int i = 0; i < amount; i++) {
+            moved.addLast(stacks.get(from).removeFirst());
+        }
+
+        for (int i = 0; i < amount; i ++) {
+            stacks.get(to).addFirst(moved.removeLast());
+        }
     }
 
     void add(int to, String added) {
-        stacks.get(to).addLast(added);
+        stacks.get(to).addFirst(added);
     }
 
     String getSol() {
@@ -89,5 +151,14 @@ class Stacks {
         }
 
         return result;
+    }
+
+    void print() {
+        for (int i = 0; i < stacks.size(); i++) {
+            System.out.print("Stack "+i+": ");
+            for (String s: stacks.get(i)) {
+                System.out.println(" "+s+" ");
+            } System.out.println("");
+        }
     }
 }
