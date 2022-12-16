d3c38364a7c98ba6e389e64f3229ba3f5244b303
OldManTanoa <zogg.ryan@gmail.com>
2022-12-16 16:09:19 +0100

    day 08 start

---
+++ day08/treeVisibility.java
@@ -0,0 +1,27 @@
+package day08;
+
+public class treeVisibility {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isVisible(int[][] grid, int x, int y) {
+        for (int i = 0; i < grid[x].length; i++) {
+                
+        }
+
+        return false;
+    }
+}
+
+class Tree{
+    int x;
+    int y;
+    int value;
+
+    public Tree(int x, int y, int value) {
+        this.x = x;
+        this.y= y; this.value = value;
+    }
+}
